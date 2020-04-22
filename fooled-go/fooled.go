43563ee7f3f77fd182c2a585d61b99dec8528db7
Olivier Richard <olivier.richard@imag.fr>
2020-04-22 19:41:11 +0200

    rename fooled

--- fooled-go/fooled.go
+++ fooled-go/fooled.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	now := time.Now()
 	t := fmt.Sprintf("%f",float64(now.UnixNano())/1e9)
-	//time.Sleep(10 * time.Second)
+	time.Sleep(1 * time.Second)
 	fmt.Println("Hello, world.")
 	fmt.Println("Now: ", now)
 	fmt.Println("now from epoch: ", t)
