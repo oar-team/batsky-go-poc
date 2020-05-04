a102c43cdc82b69defae79696b05b58371c7b549
Théo Larue <thlarue@live.fr>
2020-05-04 10:53:50 +0200

    improved zmq exchanges

--- broker-go/broker.go
+++ broker-go/broker.go
@@ -4,32 +4,23 @@ import (
 	"encoding/binary"
 	"encoding/json"
 	"fmt"
-	"time"
 
 	zmq "github.com/pebbe/zmq4"
 )
 
 func main() {
-	responder, _ := zmq.NewSocket(zmq.REP)
-	handshake, _ := zmq.NewSocket(zmq.REQ)
-	defer responder.Close()
-	defer handshake.Close()
-	responder.Bind("tcp://127.0.0.1:27000")
-	handshake.Bind("tcp://127.0.0.1:27001")
+	requester, _ := zmq.NewSocket(zmq.REQ)
+	defer requester.Close()
+	requester.Bind("tcp://127.0.0.1:27000")
 
 	now := float64(0)
 
 	for {
 		// Tell time we're ready to receive
-		_, err := handshake.SendBytes([]byte("ready"), 0)
-		handshakeReplyBytes, _ := handshake.RecvBytes(0)
-		handshakeReply := string(handshakeReplyBytes)
-		if handshakeReply != "ok" {
-			panic(fmt.Sprintf("Failed handshake : Expected %s, got %s", "ok", handshakeReply))
-		}
+		_, err := requester.SendBytes([]byte("ready"), 0)
 
 		//  Wait for next request from client
-		msg, err := responder.RecvBytes(0)
+		msg, err := requester.RecvBytes(0)
 		durations := make([]int64, 0)
 		if err != nil {
 			panic("Error receiving message:" + err.Error())
@@ -43,22 +34,39 @@ func main() {
 
 		for _, d := range durations {
 			if d > 0 {
-				fmt.Printf("call me later %f\n", now+float64(d/1e6)/1e3)
+				//fmt.Printf("call me later %f\n", now+float64(d/1e6)/1e3)
 			}
 		}
 
 		reply := uint64(now * 1e9)
 		b := make([]byte, 8)
 		binary.LittleEndian.PutUint64(b, reply)
-		_, err = responder.SendBytes(b, 0)
+		_, err = requester.SendBytes(b, 0)
 		if err != nil {
 			panic("Error sending message: " + err.Error())
 		}
 
+		//if int64(now*1e3)%1e3 == 0 {
+		//	fmt.Println("now:", now)
+		//}
+		fmt.Println("now", now)
 		// now is in seconds and its precision is in milliseconds
-		fmt.Println("now:", now)
-		deltams := int64(10)
+		// Minimum resolution is 2ms. 1ms introduces conversion problems.
+		deltams := int64(2)
+		//fmt.Println("now*1000", now*1000)
+		//fmt.Println("int64", int64(now*1000))
+		//fmt.Println("+delta", int64(now*1000)+deltams)
+		//fmt.Println("float64", float64(int64(now*1000)+deltams))
+		//fmt.Println("/1000", float64(int64(now*1000)+deltams)/1000)
 		now = float64((int64(now*1000) + deltams)) / 1000
-		time.Sleep(time.Millisecond * 10)
+
+		msg, err = requester.RecvBytes(0)
+		if err != nil {
+			panic(err)
+		}
+		done := string(msg)
+		if done != "done" {
+			panic(fmt.Sprintf("Failed exchange: Expected %s, got %s", "done", done))
+		}
 	}
 }
